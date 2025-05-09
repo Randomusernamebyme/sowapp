9fa2b373ce5caf4adff86cb6801e32a5c919f2fd
Randomusernamebyme <jiayiannie@gmail.com>
2025-05-10 01:07:05 +0800

    apikey

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,13 +4,13 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "你的API金鑰",
-  authDomain: "你的Auth網域",
-  projectId: "你的projectId",
-  storageBucket: "你的storageBucket",
-  messagingSenderId: "你的messagingSenderId",
-  appId: "你的appId",
-  measurementId: "你的measurementId",
+  apiKey: "AIzaSyAm9BzPvU4qPVQJ7lD7Fry7ccvvZeVRG8s",
+  authDomain: "sowapp-3d85f.firebaseapp.com",
+  projectId: "sowapp-3d85f",
+  storageBucket: "sowapp-3d85f.firebasestorage.app",
+  messagingSenderId: "715816519247",
+  appId: "1:715816519247:web:cc8d96f6a5e3e43c1b4c3a",
+  measurementId: "G-5Q20H356RJ"
 };
 
 // 初始化 Firebase
